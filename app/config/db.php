ecd1628e8379b6f514d197fb7d0f6631ac9ce1fb
DanilchenkoK <49709833+DanilchenkoK@users.noreply.github.com>
2019-11-27 16:17:32 +0300

    Update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'host'=>'localhost',
-    'name'=>'j95080dl_books',
-    'password'=>'Qazxsw!@#$',
-    'user'=>'j95080dl_books',
+    'name'=>'*********',
+    'password'=>'*********',
+    'user'=>'*********',
 ];
 ?>
